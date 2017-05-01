5e93d52f6d0afe633e6d3300e3c9d0ca5df117c6
Anthony Hernandez <tony91139@gmail.com>
2017-04-30 20:10:11 -0700

    Update root page to point to Omnivision#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file
+  # see http://guides.rubyonrails.org/routing.html
+
+  # omnivision will be primary offering
+  # an easy means to quickly determine
+  # what your opponent could play with
+  # their current board state
+  root 'omnivision#index'
 end
